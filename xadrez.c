ed901309335bec613e18e5d6e6a3a42a2f5a5dbd
Willianx19 <williansoares2015@outlook.com.br>
2025-05-21 17:25:08 +0000

    Desafio - Mestre

--- xadrez.c
+++ xadrez.c
@@ -9,7 +9,7 @@ void recursivo(int bispo)
     {
     printf("Bispo para cima \n");
 
-    for(int j = 0; j < 2; j++)
+    for(int j = 0; j < 1; j++)
     {
     printf("Bispo: para direita \n");
     recursivo(bispo -1);
@@ -17,20 +17,53 @@ void recursivo(int bispo)
     }
     }
     }
-}
+    }
 
+//Movimento da peça torre com recursivo;
 void recursivo2(int torre)
 {
 
-}
+    if(torre > 0)
+    {
+    printf("Torre: para direita \n");
+    recursivo2( torre -1);
+
+    }
+    }
+
+//Movimento da peça torre com recursivo;
+void recursivo3 (int rainha)
+{
+ if(rainha > 0)
+{
+    printf("Rainha: para esquerda \n");
+    recursivo3(rainha -1);
+    }
+    }
 
 int main() {
 
 //Variaveis
 int movimento = 5;
+int movimento2 = 5;
+int movimento3 = 8;
 
 recursivo(movimento);
+recursivo2(movimento2);
+recursivo3(movimento3);
 
 
+/*
+//Peça Cavalo com loops aninhados com multiplas variáveis:
+for(int i = 0, j = 3; i < 1 && j > 2; i++, j--)
+{
+    printf("Cavalo: para direito \n");
+
+for(int cavalo = 0, casas = 4; cavalo < 2 && casas > 2; cavalo++,casas--)
+{
+    printf("Cavalo: para cima \n");
+    }
+    }
+*/
     return 0;
 }
